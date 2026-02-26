d40f873bbebdb20cbecdaecac88d53581c79c696
Muhammad Omer Faisal <161882699+DataWithOmer@users.noreply.github.com>
2026-02-26 20:13:33 +0500

    Change database connection to PostgreSQL
    Updated the database connection function to use PostgreSQL instead of SQL Server.

--- streamlit_dashboard/db_conn.py
+++ streamlit_dashboard/db_conn.py
@@ -1,16 +1,10 @@
 import streamlit as st
-import pyodbc # Used SQL Server as the db for the streamlit dashboard
 
 def get_connection():
-    # Retrieve credentials from .streamlit/secrets.toml
-    creds = st.secrets["connections"]["sqlserver"]
-    
-    conn_str = (
-        f"DRIVER={{{creds['driver']}}};"
-        f"SERVER={creds['server']};"
-        f"DATABASE={creds['database']};"
-        "Trusted_Connection=yes;"
-        "TrustServerCertificate=yes;" 
-    )
-
-    return pyodbc.connect(conn_str)
+    """
+    Initializes a connection to the PostgreSQL database 
+    using the credentials in .streamlit/secrets.toml
+    """
+    # This automatically looks for [connections.postgresql] in secrets.toml
+    conn = st.connection("aiven_db", type="sql")
+    return conn
