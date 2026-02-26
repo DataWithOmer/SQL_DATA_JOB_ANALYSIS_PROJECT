e5719a51e17f09a253a75d2a136ceae899d90b83
Muhammad Omer Faisal <161882699+DataWithOmer@users.noreply.github.com>
2026-02-26 22:11:54 +0500

    Simplify get_connection function
    Removed docstring from get_connection function.

--- streamlit_dashboard/db_conn.py
+++ streamlit_dashboard/db_conn.py
@@ -1,10 +1,7 @@
 import streamlit as st
 
 def get_connection():
-    """
-    Initializes a connection to the PostgreSQL database 
-    using the credentials in .streamlit/secrets.toml
-    """
     # This automatically looks for [connections.postgresql] in secrets.toml
     conn = st.connection("aiven_db", type="sql")
     return conn
+
