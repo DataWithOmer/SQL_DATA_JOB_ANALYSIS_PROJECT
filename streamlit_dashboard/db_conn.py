3e1d6bb9dfa654ee09f29b1f6abd7b1566fc968a
Muhammad Omer Faisal <161882699+DataWithOmer@users.noreply.github.com>
2026-02-26 22:12:11 +0500

    Update comment for get_connection function

--- streamlit_dashboard/db_conn.py
+++ streamlit_dashboard/db_conn.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 def get_connection():
-    # This automatically looks for [connections.postgresql] in secrets.toml
+    # This looks for [connections.postgresql] in secrets.toml
     conn = st.connection("aiven_db", type="sql")
     return conn
+
 
