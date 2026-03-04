7f77b25d2a0f1125036c42105fcfb37dedb8d255
Muhammad Omer Faisal <161882699+DataWithOmer@users.noreply.github.com>
2026-03-05 01:02:08 +0500

    Update App.py

--- streamlit_dashboard/App.py
+++ streamlit_dashboard/App.py
@@ -226,7 +226,7 @@ elif page == "📊 Market Overview":
         fig_bar.update_layout(font=dict(weight='bold'), margin=dict(t=30, b=10), coloraxis_showscale=True)
         st.plotly_chart(fig_bar, use_container_width=True)
     
-    st.markdown('<hr style="margin-top:0px; margin-bottom:38px; border: 1px solid rgba(255,255,255,0.1)">', unsafe_allow_html=True)
+    st.markdown('<hr style="margin-top:-3px; margin-bottom:42px; border: 1px solid rgba(255,255,255,0.1)">', unsafe_allow_html=True)
     
       # Scatter Plot with Market Average 
     col_header, col_switch = st.columns([4, 1])
@@ -358,7 +358,7 @@ elif page == "💰 Salary Insights":
         st.plotly_chart(fig_salary, use_container_width=True)
     else:
         st.info(f"No salary data available for selected filters.")
-    st.markdown('<hr style="margin-top:-5px; margin-bottom:35px; border: 1px solid rgba(255,255,255,0.1)">', unsafe_allow_html=True)
+    st.markdown('<hr style="margin-top:-5px; margin-bottom:39px; border: 1px solid rgba(255,255,255,0.1)">', unsafe_allow_html=True)
         
     # Chart: Highest Average Salaries By Role
     col_header, col_switch = st.columns([4, 1])
@@ -421,5 +421,6 @@ elif page == "Skill Economics":
 elif page == "Top Hiring Companies":
     st.title("🏢 Top Hiring Companies")
     st.write("🏢 Market Leaders: Top Hiring Companies")
+
 
 
